22525dd157940d14b05a8719b8c7c753927f8d67
beta-fish <Sasha@Sashas-MacBook-Air.local>
2016-03-20 11:21:49 -0700

    login

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 var dataRef = new Firebase('https://cat-chat.firebaseio.com/');
 
+//login
+
+var userName
+
+$('#login-google').click(function()
+{
+    dataRef.authWithOAuthPopup("google", function(error, authData) {
+        if (error) {
+            console.log("Login Failed!", error);
+        } else {
+            console.log("Authenticated successfully with payload:", authData);
+            
+            status = "logout"
+        }
+        
+        $('#profile-name').prepend(authData.google.displayName);
+        
+        $('#profile-image').attr('src',authData.google.profileImageURL);
+        
+        userName = authData.google.displayName;
+    });
+});
+
 $('#submit').click(function() {
     
     var message = $('#message').val();
     
-    dataRef.push({message: message});
+    dataRef.push({message: userName + " Says: " + message});
     
     $('#message').val('');
 })
